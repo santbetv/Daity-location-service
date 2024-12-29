cf9a569767164e0209a4a80b4bc5a05d47d95f41
santbetv <santiago-betancur@hotmail.com>
2024-12-29 03:58:54 -0500

    fix: adding Config CORS

--- src/main/java/com/deity/location/config/CORSConfig.java
+++ src/main/java/com/deity/location/config/CORSConfig.java
@@ -1,20 +1,30 @@
 package com.deity.location.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 @Configuration
-public class CORSConfig implements WebFluxConfigurer {
+public class CORSConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Aplica a todas las rutas
-                .allowedOrigins("*") // Permite cualquier origen
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // Métodos permitidos
-                .allowedHeaders("*") // Permite todos los encabezados
-                .exposedHeaders("Authorization") // Encabezados visibles
-                .allowCredentials(true) // Permite enviar credenciales
-                .maxAge(3600); // Cache por 1 hora
+    //This Handlers implement the CorsConfigurationSource interface in order to provide a CorsConfiguration for each request.
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cc = new CorsConfiguration();
+        cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization"));
+        cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        cc.setAllowedOrigins(Arrays.asList("/*"));
+        cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "PATCH"));
+        cc.addAllowedOriginPattern("*");
+        cc.setMaxAge(Duration.ZERO);
+        cc.setAllowCredentials(Boolean.TRUE);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cc);
+        return source;
     }
 }
