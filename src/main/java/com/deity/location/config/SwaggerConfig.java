9f02c84879f4f754e7ac4488b72d1029fbb288b2
santbetv <santiago-betancur@hotmail.com>
2024-12-29 04:35:36 -0500

    fix: adding Config SwaggerConfig

--- src/main/java/com/deity/location/config/SwaggerConfig.java
+++ src/main/java/com/deity/location/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .components(new Components())
                 .info(apiEndPointinfo())
+                .addServersItem(new Server().url("https://daity-location-service-production.up.railway.app").description("Generated server url"))
                 .externalDocs(new ExternalDocumentation()
                         .description("Spring Location Wiki Documentation")
                         .url("https://springshop.wiki.github.org/docs"));
