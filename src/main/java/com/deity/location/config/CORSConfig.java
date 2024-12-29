1fb0a1c0ba851439d58738698701451043c9b358
santbetv <santiago-betancur@hotmail.com>
2024-12-29 03:49:06 -0500

    fix: adding Config CORS

---
+++ src/main/java/com/deity/location/config/CORSConfig.java
@@ -0,0 +1,20 @@
+package com.deity.location.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CORSConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas las rutas
+                .allowedOrigins("*") // Permite cualquier origen
+                .allowedMethods("GET") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .exposedHeaders("Authorization") // Encabezados visibles
+                .allowCredentials(true) // Permite enviar credenciales
+                .maxAge(3600); // Cache por 1 hora
+    }
+}
